794304a4645c87806fbbeb8ec661e039bc18dc83
dave bl <db@d1b.org>
2010-12-20 19:36:27 +1100

    accept a filename as a parm and ensure that the parser is given one
    Signed-off-by: dave bl <db@d1b.org>

--- shows_hosts_with_open_port_and_service_desc.py
+++ shows_hosts_with_open_port_and_service_desc.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from lxml import etree
+from optparse import OptionParser
+from sys import exit
 
-def main():
-	doc = etree.parse("scan.xml")
+def main(file_name):
+	doc = etree.parse(file_name)
 	for x in doc.xpath("//host[ports/port[state[@state='open']]]"):
 		for addr in x.xpath("address/@addr"):
 			print addr
@@ -12,6 +14,14 @@ def main():
 
 
 
-if __name__ == "__main__":
-	main()
+def parse_opts():
+	parser = OptionParser()
+	parser.add_option("-f", "--file-name", action="store", dest="file_name", help = "the filename of the nmap scan")
+	(options, args) = parser.parse_args()
+	if options.file_name is None:
+		print "you MUST enter a file name, see -h"
+		exit(1)
+	main(options.file_name)
 
+if __name__ == "__main__":
+	parse_opts()
